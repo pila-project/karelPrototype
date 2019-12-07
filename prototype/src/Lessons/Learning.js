2a72e4114ff7c8fefe9cc4b3aab3c7993b7d6967
Richard Davis <richard.lee.davis@gmail.com>
2019-12-06 20:11:35 -0800

    Added an example instructional sequence to the learning page.

--- prototype/src/Lessons/Learning.js
+++ prototype/src/Lessons/Learning.js
@@ -17,6 +17,13 @@ class Learning extends Component {
           <li>Work through a series of examples and problems that incrementally teach CS concepts.</li>
           <ul>
             <li>Each concept will have three tabbed views: the problem, relevant examples, and resources (e.g., videos)</li>
+            <li>Types of problems/exercises</li>
+              <ul>
+                <li>Completing partial examples</li>
+                <li>Modifying existing code</li>
+                <li>Make predictions about code execution</li>
+                <li>Make changes to the environment so code executes correctly (e.g., clicking to add stones)</li>
+              </ul>
           </ul>
         </ol>
         
@@ -37,10 +44,45 @@ class Learning extends Component {
           <li>...</li>
         </ol>
 
-        <h3>Additional Comments and Questions</h3>
-        <ul>
-          <li>...</li>
-        </ul>
+        <h3>Example Instructional Sequence</h3>
+          <ol>
+            <li>Karel, Stone, World, Wall</li>
+            <li>move(), turnLeft(), putBeeper(), pickBeeper()</li>
+            <li>Programs as sequences of commands (no control flow)</li>
+            <ul>
+              <li>Change program so Karel moves three steps</li>
+              <li>Change program so Karel gets to top of ledge</li>
+              <li>Change program so Karel puts a stripe of stones without running into wall</li>
+              <li>Change program so Karel picks up a stack of stones</li>
+            </ul>
+            <li>Functions</li>
+            <ul>
+              <li>Demo: turnRight()</li>
+              <li>Demo: turnAround()</li>
+            </ul>
+            <li>Write a program from scratch</li>
+            <ul>
+              <li>Get the newspaper: First chance to measure knowledge of functions</li>
+            </ul>
+            <li>Repeat</li>
+            <ul>
+              <li>Demo: Put a beeper in each corner using repeat</li>
+              <li>Write: Place 50 stones</li>
+            </ul>
+            <li>While and Special functions</li>
+            <ul>
+              <li>Demo: Use beepersPresent() to pick up an arbitrary number of beepers</li>
+              <li>Write: Move forward until encountering a wall, no matter how big, by using frontIsClear()</li>
+              <li>Change: Fix putBeeperLine() function by adding precondition putBeeper()</li>
+              <li>Demo: Nested while loops to pick up any beepers on first row</li>
+              <li>Write: Fill the entire world with beepers. Given putBeeperLine() to start. This is tricky with limited function set.</li>
+            </ul>
+            <li>If/Else Statements</li>
+            <ul>
+              <li>Demo: Use if/else to invert beepers</li>
+              <li>Write: Rebuild columns found on the first row</li>
+            </ul>
+          </ol>
       </div>
     )
   }
