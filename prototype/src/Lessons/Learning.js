8e407c5b61bf027dc9f2fca75f6115a99dee0d06
Richard Davis <richard.lee.davis@gmail.com>
2019-12-06 20:01:32 -0800

    Added some text to learning and posttest pages. Definitely unfinished.

--- prototype/src/Lessons/Learning.js
+++ prototype/src/Lessons/Learning.js
@@ -9,7 +9,38 @@ class Learning extends Component {
   render() {
     return (
       <div className="verticalContainer">
-        <p>Learning Phase</p>
+      < h1>Lesson Description</h1>
+        <br />
+
+        <h3>What Students Do</h3>
+        <ol>
+          <li>Work through a series of examples and problems that incrementally teach CS concepts.</li>
+          <ul>
+            <li>Each concept will have three tabbed views: the problem, relevant examples, and resources (e.g., videos)</li>
+          </ul>
+        </ol>
+        
+        <h3>What Is Measured</h3>
+        <ol>
+          <li>During each problem</li>
+          <ul>
+            <li>Speed</li>
+            <li>Accuracy</li>
+            <li>Resources used</li>
+            <li>Programs created and tested</li>
+            <li>Debugging strategies (e.g., stepping through code, adding and removing blocks)</li>
+          </ul>
+        </ol>
+
+        <h3>Uses for Outcome Measures</h3>
+        <ol>
+          <li>...</li>
+        </ol>
+
+        <h3>Additional Comments and Questions</h3>
+        <ul>
+          <li>...</li>
+        </ul>
       </div>
     )
   }
--- prototype/src/Lessons/PostTest.js
+++ prototype/src/Lessons/PostTest.js
@@ -9,7 +9,35 @@ class PostTest extends Component {
   render() {
     return (
       <div className="verticalContainer">
-        <p>Post-Test Phase</p>
+      <h1>Post-Test Description</h1>
+        <br />
+
+        <h3>What Students Do</h3>
+        <ol>
+          <li>Open-ended challenges (Details?)</li>
+          <li>Code prediction questions to match pretest</li>
+          <li>Questions about feelings of anxiety</li>
+        </ol>
+        
+        <h3>What Is Measured</h3>
+        <ol>
+          <li>Measure how much students are able to learn</li>
+          <li>Measure changes in knowledge due to lesson</li>
+          <li>Measure changes in technical anxiety</li>
+        </ol>
+
+        <h3>Uses for Outcome Measures</h3>
+        <ol>
+          <li>How far students get provides a measure of learning ability (among other things)</li>
+          <li>Changes from pre to post on matched questions provides a measure of what students learned</li>
+          <li>Changes from pre to post anxiety</li>
+        </ol>
+
+        <h3>Additional Comments and Questions</h3>
+        <ul>
+          <li>...</li>
+        </ul>
+
       </div>
     )
   }
