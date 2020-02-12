2db74855e762376a42538026baefcccaf5f99cf0
Richard Davis <richard.lee.davis@gmail.com>
2020-02-12 02:43:46 -0800

    Added lazy loading for studentState in react store.

--- prototype/src/redux/reducers/index.js
+++ prototype/src/redux/reducers/index.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import { UPDATE_STATUS, UPDATE_CODE, UPDATE_CURRENT_ID } from "../actionTypes";
-import { STATUS, VIEW } from "../../constants"
+import { STATUS, VIEW, IDs } from "../../constants"
 
 const initialState = {
     currentID: null,
-    studentState: {
-        learnCmd1: {
-            status:STATUS.UNVISITED,
-            code:''
-        },
-        learnCmd2: {
-            status:STATUS.UNVISITED,
-            code:''
-        }
-    }
+    studentState: {} // Lazy load
+    
+    // Example of what studentState will look like with content
+    // studentState: {
+    //     learnCmd1: {
+    //         status:STATUS.UNVISITED,
+    //         code:''
+    //     },
+    //     learnCmd2: {
+    //         status:STATUS.UNVISITED,
+    //         code:''
+    //     }
+    // }
+
 };
 
 // Reducers can't update the redux store directly because it is immutable.
@@ -21,7 +25,7 @@ const initialState = {
 // All of the spread operators below are being used to create a copy of the state
 // and selectively modify the part that needs to be changed based 
 // on the action that was received. For example, when UPDATE_STATUS is 
-// received, the `status` key of a specific item is updated.
+// received, the `status` value of the item specified by state.currentID is updated.
   
 function rootReducer(state = initialState, action) {
     switch (action.type) {
